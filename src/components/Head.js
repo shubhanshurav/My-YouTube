5f3e1e219b6da0b8a77b415e972b731db729b1b5
shubhanshurav <shubhanshurao7037@gmail.com>
2023-10-29 22:25:43 +0530

    Add live chat feauture

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,8 +1,8 @@
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleMenu } from '../utils/appSlice';
-// import { YOUTUBE_SEARCH_API } from '../utils/Contants';
-// import { cacheResults } from '../utils/searchSlice';
+import { YOUTUBE_SEARCH_API } from '../utils/Contants';
+import { cacheResults } from '../utils/searchSlice';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {BiUserCircle, BiVideoPlus} from 'react-icons/bi';
 import {BsMic} from 'react-icons/bs';
@@ -12,18 +12,18 @@ import {IoMdNotificationsOutline} from 'react-icons/io';
 const Head = () => {
 
   // For Seraching
-  // const[searchQuery, setSearchQuery] = useState("");
-  // const[suggestions, setSuggestions] = useState([]);
-  // const[showSuggestions, setShowSuggestions] = useState(false);
+  const[searchQuery, setSearchQuery] = useState("");
+  const[suggestions, setSuggestions] = useState([]);
+  const[showSuggestions, setShowSuggestions] = useState(false);
   
-  // const searchCache = useSelector((store) => store.search);
+  const searchCache = useSelector((store) => store.search);
 
   const dispatch = useDispatch();
 
   //make an API call after any key press
   // but if difference between 2 API calls is < 200ms
   //Decline the API Call
- /**
+ 
   useEffect(() => {
 
     const timer = setTimeout(() =>  {
@@ -39,7 +39,7 @@ const Head = () => {
     };
 
   },[searchQuery]);
-*/
+
 
 
 /** 
@@ -60,7 +60,7 @@ const Head = () => {
 */
 
   // API Call
-  /**
+ 
   const getSearchSuggestions = async() => {
     // console.log("API Call: ", searchQuery);
 
@@ -82,7 +82,7 @@ const Head = () => {
       console.error('Error:',error);
     }
   };
-  */
+  
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -114,10 +114,10 @@ const Head = () => {
                 placeholder='Search'
                 className='px-5 w-[90%] border borger-zinc-600 p-2 rounded-l-full bg-zinc-800'
                 // For Searching in Search Bar
-                // value={searchQuery}
-                // onChange={(e) => setSearchQuery(e.target.value)}
-                // onFocus={() => setShowSuggestions(true)}
-                // onBlur={() => setShowSuggestions(false)}
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                onFocus={() => setShowSuggestions(true)}
+                onBlur={() => setShowSuggestions(false)}
               />
               <button 
               className='border border-gray-400 lg:px-5 md:px-3 sm:px-2 p-2 bg-black rounded-r-full'>
@@ -131,7 +131,7 @@ const Head = () => {
               <BsMic />
             </div>
           </div>
-          {/* {showSuggestions && (
+          {showSuggestions && (
            <div className='fixed bg-white py-2 px-2 w-[26rem] shadow-lg rounded-lg border border-gray-100'>
              <ul>
                 {suggestions.map((s) => (
@@ -140,7 +140,7 @@ const Head = () => {
                   </li>
                   ))}
              </ul>
-           </div>)} */}
+           </div>)}
         </div>
 
         <div className='flex items-center gap-5 col-span-1 '>
