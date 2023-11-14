a21fd21d082bc17d2d8c4b9b51ff4c7f5accc38c
shubhanshurav <shubhanshurao7037@gmail.com>
2023-11-14 19:28:47 +0530

    Make Responsive for smartphones

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -15,25 +15,24 @@ const WatchPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-row w-[100%]">
+    <div className="flex md:flex-row flex-col w-[100%]">
       <div className="flex flex-col px-2 w-[65%]">
         <div> 
           <iframe
-            width="800"
-            height="500"
+            className="w-[390px] md:w-[800px] h-[300px] md:h-[500px]"
             src={"https://www.youtube.com/embed/" + searchParams.get('v')}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
-          >         
+          >
           </iframe>
         </div>
-        <div className="w-full h-screen overflow-y-auto">
+        <div className="w-screen md:w-full h-screen overflow-y-auto">
            <CommentsContainer />
         </div>
       </div>
-      <div className="w-[33%]">
+      <div className="w-screen px-5 md:px-0 md:w-[33%]">
         <div>
           <LiveChat />
         </div>
